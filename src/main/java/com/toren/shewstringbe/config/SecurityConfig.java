26d1ea98f4a26872954dd56a27175db11a812c46
TorenR <torentremaine@outlook.com>
2024-10-12 23:19:00 -0400

    CORS configured to allow request from local frontend

--- src/main/java/com/toren/shewstringbe/config/SecurityConfig.java
+++ src/main/java/com/toren/shewstringbe/config/SecurityConfig.java
@@ -16,6 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -34,7 +40,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(CsrfConfigurer::disable).authorizeHttpRequests(req -> req.requestMatchers("/user/*").permitAll()
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(req -> req.requestMatchers("/user/*").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
@@ -50,6 +57,19 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
--- src/main/java/com/toren/shewstringbe/dto/bankaccountdto/BankAccountTransactionsMappedDto.java
+++
@@ -1,7 +0,0 @@
-package com.toren.shewstringbe.dto.bankaccountdto;
-
-import com.toren.shewstringbe.base.BankAccountBase;
-
-public class BankAccountTransactionsMappedDto extends BankAccountBase {
-
-}
