d8741c74b74683cc32160e48bb025cef0e716b97
TorenR <torentremaine@outlook.com>
2025-02-05 00:29:51 -0500

    Updated Sec Config for new deployment setup

--- src/main/java/com/toren/shewstringbe/config/SecurityConfig.java
+++ src/main/java/com/toren/shewstringbe/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:82", "http://192.168.128.86"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.128.86", "https://localhost", "https://192.168.128.86", "https://shewstring.com", "https://www.shewstring.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
